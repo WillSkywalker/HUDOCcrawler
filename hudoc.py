4b6de6e71460f8140e7f78e507c8c7762e8bfe95
X. Xu <s3612406@rechten-152-56.rechten.rug.nl>
2019-02-15 18:00:00 +0100

    init

--- hudoc.py
+++ hudoc.py
@@ -14,6 +14,9 @@ def get_document(item_id, doc_name):
         f.write(res.content)
 
 
+def get_documents(col):
+    res = s.get('https://hudoc.echr.coe.int/app/query/results?query=contentsitename%3AECHR%20AND%20(NOT%20(doctype%3DPR%20OR%20doctype%3DHFCOMOLD%20OR%20doctype%3DHECOMOLD))%20AND%20((languageisocode%3D%22ENG%22))%20AND%20((article%3D6))%20AND%20((documentcollectionid%3D%22%s%22))&select=sharepointid,Rank,ECHRRanking,languagenumber,itemid,docname,doctype,application,appno,conclusion,importance,originatingbody,typedescription,kpdate,kpdateAsText,documentcollectionid,documentcollectionid2,languageisocode,extractedappno,isplaceholder,doctypebranch,respondent,ecli,appnoparts,sclappnos&sort=&start=0&length=20&rankingModelId=11111111-0000-0000-0000-000000000000' % col)
+    length =
 
 if __name__ == '__main__':
     get_document('001-189791', 'CASE OF SA-CAPITAL OY v. FINLAND')
